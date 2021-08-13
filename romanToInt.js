5dfaec5baffcc645f544c0e924afae13eb9ad560
Aaron Williams <aaronrw4@yahoo.com>
2021-08-12 21:19:33 -0700

    Solution

--- romanToInt.js
+++ romanToInt.js
@@ -1,3 +1,28 @@
 var romanToInt = function(s) {
-    
+    let str = s.split('')
+    let roman = {
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
+        'V': 5,
+        'I': 1
+    }
+    let number = 0
+    let temp = 0
+
+    for (i = str.length-1; i >= 0; i--) {
+        if (roman[str[i]] >= temp) {
+            temp = roman[str[i]]
+            number = number + roman[str[i]]
+        } else {
+            number = number - roman[str[i]]
+        }
+    }
+
+    return number   
 };
+
+
+console.log(romanToInt('III'))
